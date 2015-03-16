b17f9b93b5909173b7834e070e35597a1f47aa22
Darron Broad <darron@kewl.org>
2015-03-16 08:50:53 +0000

    move splitmem location.
    now need to test this.

--- firmware/mk/CHIPKIT_PI_MX270_USB_48.mk
+++ firmware/mk/CHIPKIT_PI_MX270_USB_48.mk
@@ -70,7 +70,7 @@ MAP     = $(BOARD)/$(TARGET:.hex=.map)
 CFLAGS  = -Os -mips16 -mprocessor=$(CPU) -std=gnu99 -pedantic-errors -Wall -fverbose-asm -save-temps=obj
 CFLAGS += -mno-smart-io -w -ffunction-sections -fdata-sections -g3 -mdebugger -Wcast-align -fno-short-double -fframe-base-loclist
 CFLAGS += -D_BOARD_$(BOARD)_
-CLINK   = -mno-peripheral-libs -nostartfiles -nostdlib -Wl,-Tlib/MX1-2-USB-boot-linkerscript.ld,-Map=$(MAP),--gc-sections
+CLINK   = -mno-peripheral-libs -nostartfiles -nostdlib -Wl,-Tlib/MX270-USB-boot-linkerscript.ld,-Map=$(MAP),--gc-sections
 
 build:init $(TARGET)
 
@@ -86,7 +86,7 @@ $(ELF):$(AOBJECT) $(COBJECT)
 	@echo -n "[LINK] "
 	$(CC) $(CFLAGS) $(CPPFLAGS) $(AOBJECT) $(COBJECT) -o $(ELF) $(CLINK)
 
-$(COBJECT):$(CHEADER) Makefile lib/MX1-2-USB-boot-linkerscript.ld
+$(COBJECT):$(CHEADER) Makefile lib/MX270-USB-boot-linkerscript.ld
 
 program:build
 	p32 program $(TARGET)
